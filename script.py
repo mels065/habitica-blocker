3de5f542f3a2c0b933f33e839d03ed7f430e78bc
Brandon Mellus <mellusbrandon@gmail.com>
2022-01-21 21:26:10 -0500

    Kill all processes with the listed apps after 5 seconds

--- script.py
+++ script.py
@@ -1,5 +1,13 @@
 import psutil
+import time
+import os
 
 blocked_apps = open("blocked.txt").read().split("\n")
 
 print(blocked_apps)
+
+while True:
+    for app in blocked_apps:
+        if app in (process.name() for process in psutil.process_iter()):
+            os.system(f'TASKKILL /F /IM {app}')
+    time.sleep(5)
